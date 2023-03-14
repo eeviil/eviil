b7f81479e3ea082474d8af5a8b38e38aea148e58
eeviil <127745458+eeviil@users.noreply.github.com>
2023-03-14 09:51:12 +0100

    Add files via upload

---
+++ evio.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    
+        int età;
+
+        printf("inserisci la tua età: ");
+        scanf("%d", &età);
+        printf("la tua età è: %d anni\n", età);  
+    }
