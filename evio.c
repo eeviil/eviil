f96a05f7211cddf8bb3f884772a9a9b91016c1a2
eeviil <127745458+eeviil@users.noreply.github.com>
2023-03-14 10:46:09 +0100

    Add files via upload

--- evio.c
+++ evio.c
@@ -2,10 +2,11 @@
 
 int main()
 {
-    
-        int età;
+    int età;
+    char sesso ;
+    printf ("inserisci la tua età eil tuo sesso: ");
+    printf ("ïnserisci il tuo sesso: ");    
+    scanf("%d %c", &età, &sesso);
+    printf("la tua età è: %d anni\n il tuo sesso è: %c sesso\n", età, sesso);  
 
-        printf("inserisci la tua età: ");
-        scanf("%d", &età);
-        printf("la tua età è: %d anni\n", età);  
     }
