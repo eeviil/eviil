5f4757eb09e4016d279e83d33dfb6d61f7e533ea
eeviil <127745458+eeviil@users.noreply.github.com>
2023-03-14 15:21:47 +0100

    Add files via upload

--- gato.c
+++ gato.c
@@ -11,10 +11,22 @@ int main()
 
     if((x+y>z)&&(x+z>y)&&(y+z>x))
     {
-    printf("congratulazioni! hai scoperto un triangolo. appena 3000 anni in ritardo!\n");
+        printf("congratulazioni! hai scoperto un triangolo. appena 3000 anni in ritardo!\n");
+        if((x == y)&&(y == z)&&(z == x))
+        {
+            printf("ïl tuo triangolo è equilatero! ci saresti potuto arrivare anche da solo\n");
+        }
+        else if((x == y)&&(y == z)&&(z != x))
+        {
+            printf("ïl tuo triangolo è isoscele! non sei la piú brillante delle menti, vero?\n");
+        }
+        else
+        {
+            printf("il tuo triangolo è scaleno! unrelated question, hai mai imparato a leggere?\n");
+        }
     }
     else
     {
     printf("non è un triangolo! credo che il tuo sia un primato tbh\n");
-    } 
+    }    
 }
